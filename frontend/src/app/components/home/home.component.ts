9190dd3f8e08ab5a878efdd29c11e7375f9bb953
Rodrigo Santos <rodrigom376@gmail.com>
2024-03-13 17:58:00 -0300

    dropdown

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/Auth.service';
+import { City } from '../../models/City.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  cities: City[] | undefined;
+  selectedCity: City | undefined;
   constructor(public authService: AuthService) { }
 
+  ngOnInit() {
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' }
+    ];
+  }
   logout() {
     this.authService.logout();
     window.location.href = '/login';
