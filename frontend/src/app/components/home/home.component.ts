e07aff2e5b04b2a354a0ab5645cb8daaff52bd89
Rodrigo Santos <rodrigom376@gmail.com>
2024-03-12 22:03:28 -0300

    sistema logout

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/Auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor(public authService: AuthService) { }
 
+  logout() {
+    this.authService.logout();
+    window.location.href = '/login';
+  }
 }
+
+
